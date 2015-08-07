696ef1c693de46cbf9fb5c3853874b2e60e0ec4f
Cameron Swords <cgswords>
2015-08-07 09:56:10 -0400

    Seeded repo

---
+++ elm_main.elm
@@ -0,0 +1,17 @@
+module TickingPort where
+
+import Signal
+import Signal exposing ((<~), Signal)
+
+import Time
+
+import Text
+import Graphics.Element exposing (Element, show)
+
+port output : Signal String
+port output = Signal.map toString (Time.every Time.second)
+
+-- Nasty hack to make the compiler happy
+main : Element
+main = show "main"
+
---
+++ main.js
@@ -0,0 +1,8 @@
+var path = require("path");
+var Elm  = require("elm-loader");
+
+var compiledCode = Elm(path.resolve(__dirname, "elm_main.elm"), __dirname);
+
+compiledCode.emitter.on("output", function(message) {
+    console.log(message)
+});
