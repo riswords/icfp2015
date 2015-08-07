13a3cd6e24a15aeddf68108978ac802348c766a6
riswords <riswords@gmail.com>
2015-08-07 17:37:26 -0400

    Update update

--- update.elm
+++ update.elm
@@ -1,14 +1,30 @@
 module Update where
 
 import DataStructs exposing (..)
-import List        exposing (all, repeat)
+import List        exposing (all, repeat, map)
 import Util        exposing (..)
 import Hex         exposing (rotateUnit, moveUnit)
 
-clearRows : Int -> Grid -> Grid
-clearRows width = 
-  let rif    ls  = if all filled ls then repeat width Empty else ls
-  in List.map rif                     
+
+clearRows : HexModel -> HexModel
+clearRows model = 
+    let clearOneRow : List Hex -> (Int, List Hex)
+        clearOneRow row = if all filled row
+                            then (1, repeat width Empty)
+                            else (0, row)
+
+        scoredRows : List (Int, List Hex)
+        scoredRows = map clearOneRow model.grid
+
+        rowScore = sum (map fst scoredRows)
+    in { model 
+            | grid <- applyGravity (map snd scoredRows)
+            , score <- score + rowScore
+            }
+
+applyGravity : Grid -> Grid
+applyGravity grid = grid
+
 
 updateUnit : Grid -> Command -> HexUnit -> (Bool, HexUnit)
 updateUnit grid command =
@@ -16,9 +32,13 @@ updateUnit grid command =
   then rotateUnit grid command
   else moveUnit   grid command
 
-update : Command -> Model -> Model
+-- Performs the requested command if possible
+-- clears lines
+-- update the score
+update : Command -> HexModel -> HexModel
 update move model = 
   let (isPlaced, updUnit) = updateUnit model.grid move model.unit
   in if isPlaced
      then model |> clearRows |> updateScore |> spawnNewUnit
      else { model | unit <- updUnit }
+
