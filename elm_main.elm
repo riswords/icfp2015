c8f5b4e16acceea43c94775a4362b9a53c076a25
Cameron Swords <cgswords>
2015-08-07 19:39:02 -0400

    A little refactor in the main

--- elm_main.elm
+++ elm_main.elm
@@ -3,13 +3,14 @@ module ElmMain where
 import Signal
 import Signal exposing ((<~), Signal)
 
-import IO     exposing (..)
-import Tests  exposing (..)
-import Init   exposing (..)
-import Search exposing (..)
-import List   exposing (map, take, head)
-import Queue  exposing (push, empty)
-import Maybe  exposing (..)
+import IO          exposing (..)
+import Tests       exposing (..)
+import Init        exposing (..)
+import Search      exposing (..)
+import List        exposing (map, take, head)
+import Queue       exposing (push, empty, Queue)
+import DataStructs exposing (..)
+import Maybe       exposing (..)
 
 import Time
 import Debug  exposing (watch)
@@ -22,13 +23,17 @@ import Graphics.Element exposing (Element, show)
 
 -- Nasty hack to make the compiler happy
 main : Signal Element
-main = 
+main = looper()
+
+looper : () -> Signal Element
+looper = \ () ->
   let init = withDefault emptyModel <| head (initGameState (fromJson test0))
+      initModel : Running (Queue (HexModel, List Command), (List Command, Int)) 
+      initModel = More ((push empty (init, [])),([], 0))
   in  Signal.map show  <| Signal.foldp 
                            (\ i m -> case m of
                                        Done _            -> m
                                        More (queue,best) -> bfStep queue best)
-                           (More ((watch "queue" (push empty (init, []))),([], 0)))
+                           initModel
                            (Time.fps 1)
-
 
