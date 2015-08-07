3cd8814417ddd1b64e57765be55d5e39195345ce
Cameron Swords <cgswords>
2015-08-07 10:01:55 -0400

    Started hex data structure, fixed a small build error, added a makefile

--- hex.elm
+++ hex.elm
@@ -1,8 +1,10 @@
 module Hex where
 
-type Hex = Empty
-         | Filled
+type Hex    = Empty
+            | Filled
 
 type HexRow = LeftRow  (List Hex)
             | RightRow (List Hex)
+
+alias Grid  = List HexRow
 
