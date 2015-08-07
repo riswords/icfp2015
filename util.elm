661aca2857e53de43e1f0db3bbe47eb22aae508d
riswords <riswords@gmail.com>
2015-08-07 15:09:35 -0400

    Add utility to get cell from x, y, z coords

--- util.elm
+++ util.elm
@@ -1,9 +1,28 @@
 module Util where
 
 import DataStructs exposing (..)
+import Maybe exposing (withDefault)
+import List exposing (head, drop)
 
 filled : Hex -> Bool
 filled f = 
   case f of
     Empty  ->False
     Filled -> True
+
+
+get : Int -> List a -> Maybe a
+get i ls = 
+    drop i ls
+    |> head
+    |> Just
+    |> withDefault Nothing
+
+getCell : Int -> Int -> Int -> Grid -> Maybe Hex
+getCell x y z grid = 
+    let rowIndex = z
+        colIndex = x + ((z - (z % 2)) // 2)
+        row = withDefault [] (get rowIndex grid)
+    in get colIndex row
+        |> Just
+        |> withDefault Nothing
