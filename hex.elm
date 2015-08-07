5ce5b6a2d92a0295914db47828a8d1d851ab816a
riswords <riswords@gmail.com>
2015-08-07 16:31:16 -0400

    Change rotate and move to only occur if they're safe
    When a move or rotate isn't safe, the move/rotate functions return
    (False, original state). Otherwise, they return (True, updatedState).

--- hex.elm
+++ hex.elm
@@ -1,41 +1,73 @@
 module Hex where
 
 import DataStructs exposing (..)
-import List exposing (map)
+import List exposing (map, any)
+import Util exposing (getCell)
 
-rotateUnit : Command -> HexUnit -> HexUnit
-rotateUnit direction unit = 
-    let newMembers = case direction of
-                        CW  -> map rotateCellCW unit.members
-                        CCW -> map rotateCellCCW unit.members
-                        _   -> unit.members
-    in { unit | members <- newMembers }
+rotateUnit : Grid -> Command -> HexUnit -> (Bool, HexUnit)
+rotateUnit grid direction unit = 
+    let newMembers : List (Bool, HexCell)
+        newMembers = case direction of
+                        CW  -> map (rotateCellCW grid) unit.members
+                        CCW -> map (rotateCellCCW grid) unit.members
+                        _   -> map (\c -> (False, c)) unit.members
 
-rotateCellCW : HexCell -> HexCell
-rotateCellCW { x, y, z } = { x = -z, y = -x, z = -y }
+        didRotate = any ((==) True) (map fst newMembers)
+    in (didRotate, { unit | members <- map snd newMembers })
 
-rotateCellCCW : HexCell -> HexCell
-rotateCellCCW { x, y, z } = { x = -y, y = -z, z = -x }
+rotateCellCW : Grid -> HexCell -> (Bool, HexCell)
+rotateCellCW grid { x, y, z } = 
+    let newLoc = HexCell -z -x -y
+        isSafe = isCellSafe grid newLoc
+    in if isSafe
+            then (isSafe, newLoc)
+            else (isSafe, HexCell x y z)
 
+rotateCellCCW : Grid -> HexCell -> (Bool, HexCell)
+rotateCellCCW grid { x, y, z } = 
+    let newLoc = HexCell -y -z -x
+        isSafe = isCellSafe grid newLoc
+    in if isSafe
+            then (isSafe, newLoc)
+            else (isSafe, HexCell x y z)
+    
 
-moveUnit : Command -> HexUnit -> HexUnit
-moveUnit direction unit = 
-    let newMembers = map (moveCell direction) unit.members
-        newLocation = moveCell direction unit.location
-    in { unit 
-       | members <- newMembers
-       , location <- newLocation
-       }
+isCellSafe : Grid -> HexCell -> Bool
+isCellSafe grid {x, y, z} = 
+    case getCell x y z grid of
+        Nothing -> False
+        Just Filled -> False
+        Just Empty -> True
 
-moveCell : Command -> HexCell -> HexCell
-moveCell direction {x, y, z} = 
-    case direction of
-        E  -> { x = x + 1, y = y - 1, z = z }
-        W  -> { x = x - 1, y = y + 1, z = z }
-        SE -> { x = x, y = y - 1, z = z + 1 }
-        SW -> { x = x - 1, y = y, z = z + 1 }
-        _  -> { x = x, y = y, z = z}
+moveUnit : Grid -> Command -> HexUnit -> (Bool, HexUnit)
+moveUnit grid direction unit =
+    let newMembers : List (Bool, HexCell)
+        newMembers = map (moveCell grid direction) unit.members
 
+        newLoc : HexCell
+        newLoc = snd (moveCell grid direction unit.location)
+
+        didMove = any ((==) True) (map fst newMembers)
+    in if didMove
+        then (didMove, { unit 
+                        | members <- map snd newMembers
+                        , location <- newLoc
+                        })
+        else (didMove, unit)
+
+moveCell : Grid -> Command -> HexCell -> (Bool, HexCell)
+moveCell grid direction {x, y, z} =
+    let newLoc : HexCell
+        newLoc = case direction of
+                    E  -> HexCell (x + 1) (y - 1) z
+                    W  -> HexCell (x - 1) (y + 1) z
+                    SE -> HexCell x (y - 1) (z + 1)
+                    SW -> HexCell (x - 1) y (z + 1)
+                    _  -> HexCell x y z
+        isSafe = isCellSafe grid newLoc
+    in if isSafe
+            then (isSafe, newLoc)
+            else (isSafe, HexCell x y z)
 
 isRotateCommand : Command -> Bool
 isRotateCommand c =
@@ -44,8 +76,8 @@ isRotateCommand c =
     CCW -> True
     _   -> False
 
-updateUnit : Command -> HexUnit -> HexUnit
-updateUnit command =
+updateUnit : Grid -> Command -> HexUnit -> (Bool, HexUnit)
+updateUnit grid command =
   if   isRotateCommand command 
-  then rotateUnit command 
-  else moveUnit   command 
+  then rotateUnit grid command
+  else moveUnit   grid command
