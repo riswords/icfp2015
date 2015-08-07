7ccd1e081c911490b9663eff99302fae04b4cf6d
Cameron Swords <cgswords>
2015-08-07 11:02:21 -0400

    Fixed this style

--- hex.elm
+++ hex.elm
@@ -1,43 +1,43 @@
 module Hex where
 
-type Hex = 
-    Empty
+type Hex 
+    = Empty
     | Filled Cell
 
-type HexRow = 
-    LeftRow  (List Hex)
+type HexRow 
+    = LeftRow  (List Hex)
     | RightRow (List Hex)
 
 type alias Grid  = List HexRow
 
-type alias Cell = {
-    x : Int
+type alias Cell = 
+    { x : Int
     , y : Int
     }
 
-type alias Unit = {
-    members :  List Cell
+type alias Unit = 
+    { members :  List Cell
     , pivot : Cell
     }
 
-type Command = 
-    E Char
+type Command 
+    = E Char
     | W Char
     | SE Char
     | SW Char
     | CW Char
     | CCW Char
 
-type alias HexModel = {
-    id : Int
+type alias HexModel = 
+    { id : Int
     , units : List Unit
     , grid : Grid
     , sourceLength : Int
     , sourceSeed : Int
     }
 
-type alias Result = {
-    id : Int
+type alias Result = 
+    { id : Int
     , seed : Int
     , tag : String
     , solution : List Command
