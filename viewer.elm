0c2e04c0f15e3d06d7a666bb82c1a21543d4db1d
Cameron Swords <cgswords>
2015-08-07 22:24:23 -0400

    Finished hte renderer

--- viewer.elm
+++ viewer.elm
@@ -2,10 +2,11 @@ module Viewer where
 
 import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
+import Text exposing (fromString, monospace)
 import Color exposing (..)
 import List exposing (indexedMap, (::))
 
-import Util exposing (getGridHeight, getGridWidth, unitToCoordinates)
+import Util exposing (getGridHeight, getGridWidth, unitToCoordinates, cellToOffset)
 import DataStructs exposing (..)
 import Search      exposing (..)
 import Queue       exposing (push, empty, Queue, peek)
@@ -25,41 +26,60 @@ hexView maybeStuff =
 
 -- Just some important values
 hexRadius    = 20
+circleRadius = hexRadius / 2
 oneHexWidth  = hexRadius * 2 * 0.88
 oneHexHeight = hexRadius * 2 * 0.80
-xoffset      = 250
-yoffset      = 200
-
+xoffset      = 0
+yoffset      = 0
 
 evenOffset n = if (n % 2 == 1) then (oneHexWidth / 2) else 0
 
 -- Drawers
-drawUnit : HexUnit -> List Form
-drawUnit unit =
+drawUnit : Float -> Float -> HexUnit -> List Form
+drawUnit h w unit =
   let coords              = unitToCoordinates unit
+      computeXY row  hex  = ((toFloat hex) * oneHexWidth + (evenOffset (row + 1)),
+                              ((h - 1) - (toFloat row)) * oneHexHeight)
+      centerDot           = let (row, hex) = cellToOffset unit.location 
+                                (x, y)     = computeXY row hex
+                            in circle circleRadius
+                                 |> filled unitCenter
+                                 |> move (x, y)
       unitNGon (row, hex) = ngon 6 hexRadius
                               |> filled uniter
                               |> rotate (degrees 30)
-                              |> move ((toFloat hex) * oneHexWidth + (evenOffset row) - xoffset, 
-                                       (toFloat row) * oneHexHeight                   - yoffset)
-  in  List.map unitNGon coords 
+                              |> move (computeXY row hex)
+  in  List.append (List.map unitNGon coords) [centerDot]
+
 
 showModel : HexModel -> Element
 showModel model = 
-    let gridWidth = getGridWidth model.grid
-        gridHeight = getGridHeight model.grid
+    let gridWidth = toFloat <| getGridWidth model.grid
+        gridHeight = toFloat <| getGridHeight model.grid
         makeNGon rownum hexnum val =
           let color   = if val == Empty then clearGrey else yeller
               evenoff = evenOffset rownum
           in ngon 6 hexRadius
                |> filled color
                |> rotate (degrees 30)
-               |> move ((toFloat hexnum) * oneHexWidth + evenoff - xoffset, 
-                        (toFloat rownum) * oneHexHeight          - yoffset)
+               |> move ((toFloat hexnum) * oneHexWidth + evenoff,
+                        (toFloat rownum) * oneHexHeight         )
         makeNGons index row = group <| indexedMap (makeNGon index) row
         ngons : List Form
         ngons = indexedMap makeNGons (List.reverse model.grid)
-    in collage 800 800 (List.append (drawUnit model.unit)  ngons)
+        unitgons = drawUnit gridHeight gridWidth model.unit
+        collageWidth  = floor (oneHexWidth * gridWidth * 1.2)
+        collageHeight = floor (oneHexHeight * gridHeight * 1.2)
+        collageOffset = (-1.0 * oneHexWidth * gridWidth * 0.5, 
+                         -1.0 * oneHexHeight * gridHeight * 0.5)
+    in flow up
+            [ collage collageWidth collageHeight 
+                      [move collageOffset (group (List.append ngons unitgons))]
+            -- , flow right [renderString  "Unit: ", show model.unit]
+            , flow right [renderString "Score: ", show model.score]
+            ]
+
+renderString = centered << monospace << fromString
 
 
 clearGrey : Color
@@ -68,3 +88,5 @@ yeller : Color
 yeller = rgba 230 184 0 1.0
 uniter : Color
 uniter = rgba 46 184 230 1.0
+unitCenter : Color
+unitCenter = rgba 25 117 209 1.0
